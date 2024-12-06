e7d0b1991384a098781cdd9dbffcdd1eeba5d469
Stuti Shukla <134101120+stuti1410@users.noreply.github.com>
2024-12-06 12:50:03 +0000

    Create symmetric_difference.py

---
+++ Sets/Symmetric Difference/symmetric_difference.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    n = int(input())
+    Eng_roll = set(map(int, input().split()))
+    b = int(input())
+    Fre_roll = set(map(int, input().split()))
+    print(len(Eng_roll ^ Fre_roll))
