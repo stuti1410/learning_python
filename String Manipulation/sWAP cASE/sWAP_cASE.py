41995b79a9280a3ccbbad681c317631942e1bf47
Stuti Shukla <134101120+stuti1410@users.noreply.github.com>
2025-02-11 14:59:51 +0000

    Update sWAP_cASE.py

--- String Manipulation/sWAP cASE/sWAP_cASE.py
+++ String Manipulation/sWAP cASE/sWAP_cASE.py
@@ -1,10 +1,5 @@
 def swap_case(s):
-    for elem in s:
-        if elem.islower():
-            result = s.swapcase()
-        if elem.isupper():
-            result = s.swapcase()
-    return result
+    return s.swapcase()
 
 if __name__ == '__main__':
     s = input()
